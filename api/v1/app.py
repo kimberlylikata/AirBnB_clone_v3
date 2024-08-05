c521fff7e099573d0ee8a01339c7e6788f63be27
kimberlylikata <kimberleylikata@gmail.com>
2024-08-05 09:12:31 -0700

    AirBnB_clone_v3

---
+++ api/v1/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+
+from flask import Flask
+from models import storage
+from api.v1.views import app.views
+
+app = flask(__name_)
+
+app.register_blueprint(app_views)
+
+
+
+if __name__ -- '__main__'
+  HOST 
